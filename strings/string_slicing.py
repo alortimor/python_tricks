e0dc41f3255a3c9b4b9303fbf7074d6837e6541f
alortimor <pkoukoulis@gmail.com>
2018-07-17 00:22:06 +0100

    Move files to their appropriate folders

---
+++ strings/string_slicing.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+There is no difference in the parameters and 
+return values (in the case of positive outcomes) between string.find() and string.index().
+The difference lies in that index generates a ValueError and find returns -1 when the
+string is not found.
+
+"""
+print(__doc__)
+
+title = "Recipe 5: Rewriting, and the Immutable String"
+colon_position = title.find(':')
+print("Sample text is {} ".format(title))
+
+print("title.index(':') is {} and title.find(':') is {} ".format(title.index(':'), title.find(':')))
+
+print("discard_text, post_colon_text =title[:colon_position],title[colon_position+2:]")
+
+discard_text, post_colon_text =title[:colon_position],title[colon_position+2:]
+
+print("discard_text is '{}', post_colon_text is '{}' = title[:colon_position],title[colon_position+2:]".format(discard_text, post_colon_text ))
+
