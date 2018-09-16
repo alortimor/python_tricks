72af8e9171909947ab050d7f942e83ae60064c46
alortimor <pkoukoulis@gmail.com>
2018-09-16 20:23:19 +0100

    Print the type hint message that appears with mypy

--- type_hints/get_first_name.py
+++ type_hints/get_first_name.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/home/bluefrog/anaconda3/bin/python3
+
 """
 
 When pressing enter instead of entering a name, the following error occurs:
@@ -32,5 +33,27 @@ first_name: str = get_first_name(raw_name)
 # If the user didn't type anything in, use the fallback name
 if not first_name:
     first_name = get_first_name(fallback_name)
+
+print(f"Hi, {first_name}!")
+
+
+
+
+#### THIS is the above code corrected.
+
+def get_first_name(full_name: str) -> str:
+    return full_name.split(" ")[0]
+
+fallback_name2: Dict[str, str] = {
+    "first_name": "UserFirstName",
+    "last_name": "UserLastName"
+}
+
+raw_name: str = input("Please enter your name: ")
+first_name: str = get_first_name(raw_name)
+
+# If the user didn't type anything in, use the fallback name
+if not first_name:
+    first_name = get_first_name(fallback_name["first_name"])
 
 print(f"Hi, {first_name}!")
