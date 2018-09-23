54a1dd4e0c04bcaac54b3f58f41d35518be42b52
alortimor <pkoukoulis@gmail.com>
2018-09-23 22:50:55 +0100

    Replace for loop of files with list comprehension for file stat script

--- files/get_file_sizes.py
+++ files/get_file_sizes.py
@@ -4,10 +4,10 @@ import pathlib
 file_sizes = []
 print("Present working directory : {}/n".format(pathlib.Path.cwd()))
 rootdir = pathlib.Path("/home/bluefrog/sample_code/python")
-for python_file in rootdir.glob("**/*.py"):
-    fs = python_file.stat().st_size
-    file_sizes.append(fs)
-    print(python_file, fs)
+file_sizes = [p.stat().st_size for p in rootdir.glob("**/*.py")]
+#for python_file in rootdir.glob("**/*.py"):
+ #   fs = python_file.stat().st_size
+  #  file_sizes.append(fs)
 
 print("Number of files : {} ".format(len(file_sizes)))
 print("Average file size {}".format(round(sum(file_sizes)/len(file_sizes),2)))
