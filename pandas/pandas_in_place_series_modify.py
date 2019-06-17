419a1aafe967ed8b1ad818e9576229545c96e609
alortimor <pkoukoulis@gmail.com>
2019-06-17 21:46:52 +0100

    Look at pandas .concat()

--- pandas/pandas_in_place_series_modify.py
+++ pandas/pandas_in_place_series_modify.py
@@ -18,3 +18,17 @@ del(s1['f'])
 
 print("Data items can remove del(s1['f']) \n{}".format(s1))
 
+
+# one can use .concat() to combine dataframes or series's.
+# When concating a series to a dataframe, the resulting type is a dataframe
+d = pd.DataFrame([1], index=['a'])
+s=pd.Series([2], index=['b'])
+x=pd.concat([d,s])
+print(type(x))
+# one can ignore existing indexes for the dataframe and/or series being combined,as follows:
+x=pd.concat([d,s], ignore_index=True)
+# one can create a new index when contating, for example
+y=pd.concat([d,s], keys=['c','d'])
+print(y)
+
+
