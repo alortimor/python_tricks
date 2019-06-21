c46e07cfaede1e67ccc9ed9f07db9af102009967
alortimor <pkoukoulis@gmail.com>
2019-06-21 16:27:42 +0100

    How to re-order columns within a DataFrame

---
+++ pandas/manipulate_df_reorder_columns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import pandas as pd
+import numpy as np
+
+pd.set_option('display.notebook_repr_html', False)
+pd.set_option('display.max_columns', 8)
+pd.set_option('display.max_rows', 10)
+pd.set_option('display.width', 80)
+
+sp500=pd.read_csv("sp500.csv",index_col="Symbol",usecols=[0,2,3,7])
+
+# reverse the columns, by creating a new data_frame
+reversed_columns=sp500.columns[::-1]
+
+print(sp500[reversed_columns][:5])
+
