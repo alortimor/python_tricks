b5a91abcc5f306246f29351880a92139e835ac24
alortimor <pkoukoulis@gmail.com>
2019-06-28 13:58:53 +0100

    Discuss chained indexing problem and evaluation order

--- pandas/dataframe_chain_indexing_problem.py
+++ pandas/dataframe_chain_indexing_problem.py
@@ -22,7 +22,6 @@ print(df_product_idx['one']['second'])
 # 2.
 print(df_product_idx.loc[:, ('one','second')] )
 
-print(df_product_idx['one'])
 
 """
   The above 2 column access methods both yield the same results.
@@ -35,8 +34,62 @@ print(df_product_idx['one'])
   one after the other.
 
   df_product_idx.loc[:,('one','second')] on the other hand passes a nested tuple 
-  of (slice(None),('one','second')) to a single call to __getitem__.
+  of (slice(None),('one','second')) to a single call of __getitem__.
   This allows pandas to deal with this as a single entity. 
   Additionally, the order of operations can be significantly faster, and allows one to index both
   axes if desired.
 """
+
+# Assigning to a chained index can lead to unpredictable results, for example
+
+df_product_idx['one']['second']='x' # method 1 assignment
+# becomes
+# df_product_idx.__getitem__('one').__setitem__('second', 'x')
+
+# whereas
+df_product_idx.loc[:, ('one','second')]='x' # method 2 assignment
+# becomes
+# df_product_idx.loc.__setitem__((slice(None), ('one','second')),'x')
+
+"""
+  Notice that method 1 has an additional __getitem__ !
+  It is hard to predict whether the __getitem__ will return a view or a copy.
+  It all depends on the memory layout of the array at the point of the assignment, 
+  which pandas makes no gurantees about.
+  Therefore, whether the __setitem__ will modify df_product_idx or a temporary object,
+  that will be immediately discarded, is unknown.
+
+  The "SettingWithCopyWarning" warning was created to flag such potentially confusing
+  "chained" assignments, since such assignments often result in unpredicatble behaviour.
+  This is especially the case when the first selection returns a copy, which may or may not occur.
+  Hence, the "SettingWithCopyWarning" warning appears when attempting method 1 types of assignments.
+  Note therefore, such assigments should be avoided due to unpredictable behaviour.
+
+  Additionally, "SettingWithCopyWarning" is associated with further text, namely:
+    A value is trying to be set on a copy of a slice from a DataFrame.
+    Try using .loc[row_indexer,col_indexer]=value instead
+  to assist with implementing the recommended method 2 type assignment.
+
+  use this setting to ensure warning appear:
+  pd.set_option('mode.chained_assignment','warn')
+"""
+
+
+# Evaluation order often determines whether a copy or the original DataFrame is being modified.
+dfb=pd.DataFrame({'a':['one','one','two','three','two','one','six'],'c': np.arange(7)})
+print(dfb)
+
+# The following updates the original, not the copy, even though the "SettingWithCopyWarning" occurs
+dfb['c'][dfb.a.str.startswith('o')]=42
+
+# The following however updates the copy, not the original
+dfb[dfb.a.str.startswith('o')]['c']=42
+
+# Regardless, use .loc, since, as is apparent from previous discussion and examples, the outcome
+# is unpredicatble
+# for exampe (correct way):
+dfb.loc[dfb.a.str.startswith('o'), 'c']=33
+print("DataFrame updated with recommended method:\n{}".format(dfb))
+
+
+
